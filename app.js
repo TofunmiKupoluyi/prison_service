68c6d5315049078bfe3a6e9014f67c00ed5f84e2
TofunmiKupoluyi <tofunmi@live.com>
2018-03-23 15:27:43 +0400

    Patrick pull this

--- app.js
+++ app.js
@@ -4,18 +4,33 @@
 var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
-
+var cookieSession = require('cookie-session');
+  
 var app = express();
-
+  
 var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "",
     database: process.env.MYSQL_DB || "softservices"
 });
-
+  
+var homeRouter = express.Router();
 var clientRouter = express.Router();
 var adminRouter = express.Router();
-
+  
+app.use("/", homeRouter);
 app.use("/client", clientRouter);
 app.use("/admin", adminRouter);
+  
+app.use("/", express.static("./"));
+app.use("/", express.static("./node_modules"));
+app.use("/", express.static("./views"));
+app.use(cookieSession({ secret: 'randomStuff', cookie: { maxAge: 60 * 60 * 1000 } }));
+  
+app.get("/", function(req, res){
+    res.sendFile("jokes.html",{ root: __dirname+"/views" });
+});
+ 
+app.get("/client", function(req, res){
+    res.sendFile("jokes.html",{ root: __dirname+"/views" });
+});
+ 
+app.listen(process.env.PORT || 3001);
