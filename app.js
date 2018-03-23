65d692d0a36dfbead86a4f3875564fa41ef5edce
Patrick_Inshuti <pim219@nyu.edu>
2018-03-23 16:58:44 +0400

    Added Some Login and Signup

--- app.js
+++ app.js
@@ -5,38 +5,37 @@ var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var cookieSession = require('cookie-session');
-  
+
 var app = express();
-  
+
 var connection = mysql.createConnection({
     database: process.env.MYSQL_DB || "softservices"
 });
-  
+
 var homeRouter = express.Router();
 var clientRouter = express.Router();
 var adminRouter = express.Router();
-  
+
 app.use("/", homeRouter);
 app.use("/client", clientRouter);
 app.use("/admin", adminRouter);
-  
+
 app.use("/", express.static("./"));
 app.use("/", express.static("./node_modules"));
 app.use("/", express.static("./views"));
+app.use(express.static("public"));
 app.use(cookieSession({ secret: 'randomStuff', cookie: { maxAge: 60 * 60 * 1000 } }));
-  
+
 homeRouter.get("/", function(req, res){
     res.render("index.ejs");
 });
- 
+
 clientRouter.get("/", function(req, res){
     res.sendFile("jokes.html",{ root: __dirname+"/views" });
 });
 
 
- 
+
 app.listen(process.env.PORT || 3001, function(req, res){
     console.log("Server Running at 30001");
 });
-
-
