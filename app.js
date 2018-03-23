82d1535c8e969d89a7bb73373420bb18c3bd2c66
TofunmiKupoluyi <tofunmi@live.com>
2018-03-23 17:48:47 +0400

    Added database details

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var cookieSession = require('cookie-session');
 var app = express();
 
 var connection = mysql.createConnection({
-    database: process.env.MYSQL_DB || "prison_system"
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "",
+    database: process.env.MYSQL_DB || "prison_system",
+    charset: "utf8mb4",
 });
 
 var homeRouter = express.Router();
