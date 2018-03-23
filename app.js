c73cf9b09f419c8698f021f180f4e54deecb8bd9
TofunmiKupoluyi <tofunmi@live.com>
2018-03-23 17:13:54 +0400

    Patrick pull this

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ var cookieSession = require('cookie-session');
 var app = express();
   
 var connection = mysql.createConnection({
-    database: process.env.MYSQL_DB || "softservices"
+    database: process.env.MYSQL_DB || "prison_system"
 });
   
 var homeRouter = express.Router();
 var clientRouter = express.Router();
 var adminRouter = express.Router();
+var recruiterRouter = express.Router();
   
 app.use("/", homeRouter);
 app.use("/client", clientRouter);
 app.use("/admin", adminRouter);
+app.use("/recruiter", recruiterRouter);
   
 app.use("/", express.static("./"));
 app.use("/", express.static("./node_modules"));
@@ -31,6 +33,99 @@ homeRouter.get("/", function(req, res){
  
 clientRouter.get("/", function(req, res){
     res.sendFile("jokes.html",{ root: __dirname+"/views" });
+});
+
+clientRouter.post("/login", function(req, res){
+    var data ={
+        err:1,
+        res:""
+    }
+    var password = req.body.password;
+    var prisonerNumber = req.body.prisonerNumber; 
+    connection.query("select * from prisoner_info where (prisoner_number=? and password=?) limit 1 ", [prisonerNumber, password], function(err, res1){
+        if(err){
+            data.res=err;
+            res.json(data);
+        }
+        else{
+            if(res1.length > 0){
+                data.res = "Login successful";
+                data.err=0;
+                res.json(data);
+            }
+
+            else{
+                data.res= "Error";
+                res.json(data);
+            }
+        }
+    });
+
+});
+
+clientRouter.post("/register", function(req, res){
+    var data = {
+        err:1,
+        res: ""
+    }
+    if(req.body.firstName && req.body.lastName && req.body.password && req.body.prisonNumber){
+        var firstName = req.body.firstName;
+        var lastName = req.body.lastName;
+        var email = req.body.email;
+        var password = req.body.password;
+        var prisonerNumber = req.body.prisonerNumber;
+
+        connection.query("INSERT INTO prisoner_info SET first_name=?, last_name=?, email=?, password=?, prisoner_number=?, registration_status=false", [firstName, lastName, email, password, prisonerNumber], function(err, res1){
+            if(err){
+                data.res = err;
+                res.json(data);
+            }
+            else{
+                data.err= 0;
+                data.res = "Successful registration";
+                res.json(data);
+            }
+        });
+    }
+    else{
+        data.res= "Incomplete parameters";
+        res.json(data);
+    }
+
+});
+
+adminRouter.get("/", function(req, res){
+    res.render("index.ejs");
+});
+
+adminRouter.post("/completeRegistration", function(req, res){
+    var data = {
+        err:1,
+        res: ""
+    }
+    if(req.body.prisoner && req.body.birthDate && req.body.sentenceStartDate && req.body.rating){
+        var prisonerId = req.body.id;
+        var birthDate = req.body.birthDate;
+        var sentenceStartDate = req.body.sentenceStartDate;
+        var rating = req.body.rating;
+
+        connection.query("UPDATE prisoner_info SET birth_date =?, sentence_start_date = ?, rating = ?, registration_status = true WHERE id=?", [birthDate, sentenceStartDate, rating, prisonerId], function(err, res1){
+            if(err){
+                data.res= err;
+                res.json(data);
+            }
+            else{
+                data.err=0;
+                data.res= "Successfully updated";
+                res.json(data);
+            }
+        });
+    }
+    else{
+        data.res = "Incomplete parameters";
+        res.json(data);
+    }
+
 });
 
 
